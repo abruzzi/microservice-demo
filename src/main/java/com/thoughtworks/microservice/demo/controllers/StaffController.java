97cd6a1c6015256b25f56611a261f161c37914d7
Qiu Juntao <juntao.qiu@gmail.com>
2016-03-19 14:21:50 +1100

    add search api

--- src/main/java/com/thoughtworks/microservice/demo/controllers/StaffController.java
+++ src/main/java/com/thoughtworks/microservice/demo/controllers/StaffController.java
@@ -3,9 +3,7 @@ package com.thoughtworks.microservice.demo.controllers;
 import com.thoughtworks.microservice.demo.models.Staff;
 import com.thoughtworks.microservice.demo.repos.StaffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -13,8 +11,13 @@ public class StaffController {
     @Autowired
     private StaffRepository staffRepository;
 
-    @RequestMapping(value="/staff", method = RequestMethod.GET)
+    @RequestMapping(value="/staffs", method = RequestMethod.GET)
     public Iterable<Staff> fetchAll() {
         return staffRepository.findAll();
+    }
+
+    @RequestMapping(value="/staffs/{loginName}", method = RequestMethod.GET)
+    public Staff find(@PathVariable String loginName) {
+        return staffRepository.findByLoginName(loginName);
     }
 }
--- src/main/java/com/thoughtworks/microservice/demo/repos/StaffRepository.java
+++ src/main/java/com/thoughtworks/microservice/demo/repos/StaffRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StaffRepository extends MongoRepository<Staff, String> {
+    Staff findByLoginName(String loginName);
 }
