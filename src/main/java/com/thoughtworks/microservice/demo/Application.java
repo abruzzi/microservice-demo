8455bb84759c9403255cdbae5729ee82ed1ca6f0
Qiu Juntao <juntao.qiu@gmail.com>
2016-03-23 14:13:45 +1100

    add swagger api

--- src/main/java/com/thoughtworks/microservice/demo/Application.java
+++ src/main/java/com/thoughtworks/microservice/demo/Application.java
@@ -2,10 +2,41 @@ package com.thoughtworks.microservice.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("staffs")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/staffs.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("ThoughtWorks Staffs API")
+                .description("Spring REST API for ThoughtWorks Staffs")
+                .termsOfServiceUrl("")
+                .contact("Qiu Juntao")
+                .license("MIT")
+                .licenseUrl("")
+                .version("2.0")
+                .build();
     }
 }
